1792e44c27efcdec00b17ba9ac505a95917c066f
zhenwie <zhengqwster@gmail.com>
2024-10-27 20:46:07 +0800

    Update 1_kernel.sh

--- config/1_kernel.sh
+++ config/1_kernel.sh
@@ -20,5 +20,5 @@ fakeroot make-kpkg --initrd --cross-compile aarch64-linux-gnu- --arch arm64 kern
 
 # copy to working
 cp /linux/arch/arm64/boot/Image.gz /output/working
-cp /linux/arch/arm64/boot/dts/qcom/msm8916-handsome-openstick-uz801.dtb /output/working
+cp /linux/arch/arm64/boot/dts/qcom/msm8916-handsome-openstick-sp970.dtb /output/working
 cp /*.deb /output/working
