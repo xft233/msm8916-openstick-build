bb5177eb5acc61962ecbd0f91a14e6bb748600d3
zhenwie <zhengqwster@gmail.com>
2024-10-28 23:15:34 +0800

    Create test.sh

---
+++ config/test.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+mkdir -p output/working rootfs
+# prep rootfs image
+truncate -s 1024M output/working/rootfs_base.ext4
+mkfs.ext4 output/working/rootfs_base.ext4
+sleep 5
+mount output/working/rootfs_base.ext4 rootfs
+
+# debootstrap bullseye
+export DEBIAN_VERSION=bullseye
+debootstrap --foreign --arch=arm64 --include openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION rootfs http://deb.debian.org/debian/
+cp $(which qemu-aarch64-static) rootfs/usr/bin
+chroot rootfs qemu-aarch64-static /bin/bash /debootstrap/debootstrap --second-stage
+#rm rootfs/usr/bin/qemu-aarch64-static
