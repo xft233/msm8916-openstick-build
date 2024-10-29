f87e2c77049d46808fdb6aa443198688bb2b100b
zhenwie <zhengqwster@gmail.com>
2024-10-29 10:46:56 +0800

    Update 2_rootfs.sh

--- config/2_rootfs.sh
+++ config/2_rootfs.sh
@@ -18,7 +18,7 @@ mount /output/working/rootfs_base.ext4 /rootfs
 export DEBIAN_VERSION=bullseye
 debootstrap --foreign --arch=arm64 --include openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION /rootfs http://deb.debian.org/debian/
 cp $(which qemu-aarch64-static) /rootfs/usr/bin
-chroot /rootfs /bin/bash /debootstrap/debootstrap --second-stage
+chroot /rootfs /debootstrap/debootstrap --second-stage
 rm /rootfs/usr/bin/qemu-aarch64-static
 
 umount /rootfs
