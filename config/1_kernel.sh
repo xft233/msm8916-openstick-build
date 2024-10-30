94e236c630f2a42c54ef98a0c23286b494f55668
zhenwie <zhengqwster@gmail.com>
2024-10-30 18:17:21 +0800

    Update 1_kernel.sh

--- config/1_kernel.sh
+++ config/1_kernel.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 set -ex
 
-rm -rf /output/*; mkdir -p /output/working
+rm -rf ./output/*; mkdir -p ./output/working
 
 # override kernel config
-cp /config/msm8916_defconfig /linux/arch/arm64/configs/msm8916_defconfig
+cp ./config/msm8916_defconfig ./linux/arch/arm64/configs/msm8916_defconfig
 
 # compile
-cd /linux
+cd ./linux
 export CROSS_COMPILE=aarch64-linux-gnu-
 export ARCH=arm64
 make clean
@@ -19,6 +19,6 @@ make -j16
 fakeroot make-kpkg --initrd --cross-compile aarch64-linux-gnu- --arch arm64 kernel_image kernel_headers modules_image
 
 # copy to working
-cp /linux/arch/arm64/boot/Image.gz /output/working
-cp /linux/arch/arm64/boot/dts/qcom/msm8916-handsome-openstick-sp970.dtb /output/working
-cp /*.deb /output/working
+cp ./linux/arch/arm64/boot/Image.gz ./output/working
+cp ./linux/arch/arm64/boot/dts/qcom/msm8916-handsome-openstick-sp970.dtb ./output/working
+cp ./*.deb ./output/working
