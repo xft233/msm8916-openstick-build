4317baf8ef417d145f7e1985b24f339ba620b83d
zhenwie <zhengqwster@gmail.com>
2024-10-30 22:09:40 +0800

    Update 1_kernel_1.sh

--- config/1_kernel_1.sh
+++ config/1_kernel_1.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -ex
 
-cd ./linux
+cd /linux
 export CROSS_COMPILE=aarch64-linux-gnu-
 export ARCH=arm64
 make -j16
@@ -10,6 +10,6 @@ make -j16
 fakeroot make-kpkg --initrd --cross-compile aarch64-linux-gnu- --arch arm64 kernel_image kernel_headers modules_image
 
 # copy to working
-cp ./linux/arch/arm64/boot/Image.gz ./output/working
-cp ./linux/arch/arm64/boot/dts/qcom/msm8916-handsome-openstick-sp970.dtb ./output/working
-cp ./*.deb ./output/working
+cp /linux/arch/arm64/boot/Image.gz /output/working
+cp /linux/arch/arm64/boot/dts/qcom/msm8916-handsome-openstick-sp970.dtb /output/working
+cp /*.deb /output/working
