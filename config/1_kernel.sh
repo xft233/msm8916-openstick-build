40133ae69af8030f9de71f80a6c005ef507441d7
zhenwie <zhengqwster@gmail.com>
2024-10-30 22:08:43 +0800

    Update 1_kernel.sh

--- config/1_kernel.sh
+++ config/1_kernel.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 set -ex
 
-rm -rf ./output/*; mkdir -p ./output/working
+rm -rf /output/*; mkdir -p /output/working
 
 # override kernel config
-cp ./config/msm8916_defconfig ./linux/arch/arm64/configs/msm8916_defconfig
+cp /config/msm8916_defconfig /linux/arch/arm64/configs/msm8916_defconfig
 
 # compile
-cd ./linux
+cd /linux
 export CROSS_COMPILE=aarch64-linux-gnu-
 export ARCH=arm64
 make clean
