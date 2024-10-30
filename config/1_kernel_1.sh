82b2e4d57b046fc5f4ab4f80fae6a6204d6e03bc
zhenwie <zhengqwster@gmail.com>
2024-10-30 18:37:02 +0800

    Create 1_kernel_1.sh

---
+++ config/1_kernel_1.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+set -ex
+
+cd ./linux
+export CROSS_COMPILE=aarch64-linux-gnu-
+export ARCH=arm64
+make -j16
+
+# package kernel => .deb
+fakeroot make-kpkg --initrd --cross-compile aarch64-linux-gnu- --arch arm64 kernel_image kernel_headers modules_image
+
+# copy to working
+cp ./linux/arch/arm64/boot/Image.gz ./output/working
+cp ./linux/arch/arm64/boot/dts/qcom/msm8916-handsome-openstick-sp970.dtb ./output/working
+cp ./*.deb ./output/working
