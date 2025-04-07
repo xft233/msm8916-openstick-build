66cb03b9961ecd4000093dbfdd3d10f97d90f9c6
xft233 <106183235+xft233@users.noreply.github.com>
2025-04-07 14:26:32 +0800

    Update 2_rootfs.sh

--- config/2_rootfs.sh
+++ config/2_rootfs.sh
@@ -17,9 +17,5 @@ mount -o compress=zstd ./output/working/rootfs_base.btrfs ./rootfs
 
 # debootstrap bullseye
 export DEBIAN_VERSION=trixie
-debootstrap --foreign --arch=arm64 --include btrfs-progs,openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION ./rootfs http://deb.debian.org/debian/
-cp $(which qemu-aarch64-static) ./rootfs/usr/bin
-chroot ./rootfs /debootstrap/debootstrap --second-stage
-rm ./rootfs/usr/bin/qemu-aarch64-static
-
+debootstrap --arch=arm64 --include btrfs-progs,openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION ./rootfs http://deb.debian.org/debian/
 umount ./rootfs
