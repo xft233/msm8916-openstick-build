ff1c76d55feb246767ffdcdd6ca69c6272432dc8
xft233 <106183235+xft233@users.noreply.github.com>
2025-04-08 08:58:57 +0800

    Update test.sh

--- config/test.sh
+++ config/test.sh
@@ -22,7 +22,7 @@ mount --bind /dev/pts ./rootfs/dev/pts
 mount --bind /sys ./rootfs/sys
 mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 sh -c 'echo ":qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-aarch64:F" > /proc/sys/fs/binfmt_misc/register'
-
+cat /proc/sys/fs/binfmt_misc/register
 
 chroot rootfs qemu-aarch64-static /bin/bash /debootstrap/debootstrap --second-stage
 #rm rootfs/usr/bin/qemu-aarch64-static
