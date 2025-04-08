b9f2f98b63e9998114e3d6df3cb0edbd5aa0e00c
xft233 <106183235+xft233@users.noreply.github.com>
2025-04-08 09:15:47 +0800

    Update test.sh

--- config/test.sh
+++ config/test.sh
@@ -12,10 +12,6 @@ sleep 5
 mount output/working/rootfs_base.ext4 rootfs
 
 # debootstrap bullseye
-export DEBIAN_VERSION=bullseye
-debootstrap --foreign --arch=arm64 --include openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION rootfs http://deb.debian.org/debian/
-cp $(which qemu-aarch64-static) rootfs/usr/bin
-cat /proc/sys/fs/binfmt_misc/register
 mount --bind /proc ./rootfs/proc 
 mount --bind /dev ./rootfs/dev # causes device busy
 mount --bind /dev/pts ./rootfs/dev/pts
@@ -23,8 +19,14 @@ mount --bind /sys ./rootfs/sys
 mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 echo ":qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-aarch64-static:F" > /proc/sys/fs/binfmt_misc/register
 cat /proc/sys/fs/binfmt_misc/register
-mount | grep binfmt_misc
-mount
 
-chroot rootfs qemu-aarch64-static /bin/bash /debootstrap/debootstrap --second-stage
+export DEBIAN_VERSION=bullseye
+debootstrap --arch=arm64 --include openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION rootfs http://deb.debian.org/debian/
+cp $(which qemu-aarch64-static) rootfs/usr/bin
+cat /proc/sys/fs/binfmt_misc/register
+
+$ mount | grep binfmt_misc
+mount
+#
+ #chroot rootfs qemu-aarch64-static /bin/bash /debootstrap/debootstrap --second-stage
 #rm rootfs/usr/bin/qemu-aarch64-static
