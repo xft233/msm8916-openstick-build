84d08c4ca55310ed630e3d5509dc10686e6957b2
xft233 <106183235+xft233@users.noreply.github.com>
2025-04-08 08:55:03 +0800

    Update test.sh

--- config/test.sh
+++ config/test.sh
@@ -15,6 +15,14 @@ mount output/working/rootfs_base.ext4 rootfs
 export DEBIAN_VERSION=bullseye
 debootstrap --foreign --arch=arm64 --include openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION rootfs http://deb.debian.org/debian/
 cp $(which qemu-aarch64-static) rootfs/usr/bin
-cat /proc/sys/fs/binfmt_misc
+cat /proc/sys/fs/binfmt_misc/register
+mount --bind /proc ./rootfs/proc 
+mount --bind /dev ./rootfs/dev # causes device busy
+mount --bind /dev/pts ./rootfs/dev/pts
+mount --bind /sys ./rootfs/sys
+mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
+sh -c 'echo ":qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-aarch64:F" > /proc/sys/fs/binfmt_misc/register'
+
+
 chroot rootfs qemu-aarch64-static /bin/bash /debootstrap/debootstrap --second-stage
 #rm rootfs/usr/bin/qemu-aarch64-static
