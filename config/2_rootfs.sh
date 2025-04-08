d3b1a5fcc47ba4779345c939808c30eac558e728
xft233 <106183235+xft233@users.noreply.github.com>
2025-04-08 15:01:25 +0800

    Update 2_rootfs.sh

--- config/2_rootfs.sh
+++ config/2_rootfs.sh
@@ -6,6 +6,7 @@ apt-get -y update
 apt-get install -y binfmt-support qemu-user-static
 apt-get install -y debootstrap
 
+mkdir -p output/working rootfs
 #download extras
 pushd /output/working
 # git clone https://salsa.debian.org/Mobian-team/mobile-usb-networking --depth=1 && cd ./mobile-usb-networking && git checkout 056d9215bb38697b9dc59ba256fc6e904ee5923d && cd ..
