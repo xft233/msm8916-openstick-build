3219f3a530395119141507113073161a6cebf096
xft233 <106183235+xft233@users.noreply.github.com>
2025-04-08 08:45:09 +0800

    Update test.sh

--- config/test.sh
+++ config/test.sh
@@ -15,5 +15,6 @@ mount output/working/rootfs_base.ext4 rootfs
 export DEBIAN_VERSION=bullseye
 debootstrap --foreign --arch=arm64 --include openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION rootfs http://deb.debian.org/debian/
 cp $(which qemu-aarch64-static) rootfs/usr/bin
+cat /proc/sys/fs/binfmt_misc
 chroot rootfs qemu-aarch64-static /bin/bash /debootstrap/debootstrap --second-stage
 #rm rootfs/usr/bin/qemu-aarch64-static
