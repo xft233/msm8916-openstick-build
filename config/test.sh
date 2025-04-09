55bcf4a4ecca36b577e3e6d97a06f5528923fa33
xft233 <106183235+xft233@users.noreply.github.com>
2025-04-09 17:33:35 +0800

    Update test.sh

--- config/test.sh
+++ config/test.sh
@@ -18,7 +18,6 @@ mount -o compress=zstd /output/working/rootfs_base.btrfs rootfs
 
 # debootstrap bullseye
 export DEBIAN_VERSION=testing
-debootstrap --foreign --arch=arm64 --include apt-utils,dialog,btrfs-progs,openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION /rootfs http://deb.debian.org/debian/
 mount --bind /proc /rootfs/proc
 mount --bind /dev /rootfs/dev # causes device busy
 mount --bind /dev/pts /rootfs/dev/pts
@@ -26,10 +25,10 @@ mount --bind /sys /rootfs/sys
 mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 echo ":qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-aarch64-static:F" > /proc/sys/fs/binfmt_misc/register
 cp $(which qemu-aarch64-static) /rootfs/usr/bin
-chroot /rootfs /debootstrap/debootstrap --second-stage
+debootstrap --arch=arm64 --include apt-utils,dialog,btrfs-progs,openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION /rootfs http://deb.debian.org/debian/
+
 
 rm /rootfs/usr/bin/qemu-aarch64-static
-
 umount /rootfs/dev/pts
 umount /rootfs/dev
 umount /rootfs/sys
