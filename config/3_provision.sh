9325ec45496092f693a8a21cd22498b4fe927df8
xft233 <106183235+xft233@users.noreply.github.com>
2025-04-09 17:41:33 +0800

    Update 3_provision.sh

--- config/3_provision.sh
+++ config/3_provision.sh
@@ -19,8 +19,11 @@ mkdir -p /rootfs/config
 mkdir -p /rootfs/output
 mount --bind /config /rootfs/config
 mount --bind /output /rootfs/output
-
+mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
+echo ":qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-aarch64-static:F" > /proc/sys/fs/binfmt_misc/register
+cp $(which qemu-aarch64-static) /rootfs/usr/bin
 chroot /rootfs /bin/bash /config/chroot-system-setup.sh
+rm /rootfs/usr/bin/qemu-aarch64-static
 
 ########################
 # boot.img
