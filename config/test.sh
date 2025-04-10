628ed47bc97e73613c45623d5bde75decf1ffeab
xft233 <106183235+xft233@users.noreply.github.com>
2025-04-10 09:36:34 +0800

    Update test.sh

--- config/test.sh
+++ config/test.sh
@@ -25,7 +25,7 @@ mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 cp $(which qemu-aarch64-static) /rootfs/usr/bin
 echo ":qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-aarch64-static:F" > /proc/sys/fs/binfmt_misc/register
 
-debootstrap --arch=arm64 --include apt-utils,dialog,btrfs-progs,openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION /rootfs http://deb.debian.org/debian/
+debootstrap --arch=arm64 --include debhelper,apt-utils,dialog,btrfs-progs,openssh-server,nano,wget,initramfs-tools,cron,wpasupplicant,init,dbus,dnsmasq,ca-certificates,gawk $DEBIAN_VERSION /rootfs http://deb.debian.org/debian/
 
 umount /proc/sys/fs/binfmt_misc
 umount /rootfs/dev/pts
